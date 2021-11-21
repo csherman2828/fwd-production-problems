e5516fb21de0edf44a08ec4035042fea7b25e6df
csherman2828 <csherman2828@gmail.com>
2021-11-21 11:33:52 -0600

    Add solution to problem 3; use typeof and isNaN to handle odd input

--- pp-04/site.js
+++ pp-04/site.js
@@ -26,18 +26,33 @@
   can accurately test your work.
 */
 
-let x = 5;
-
-let double = function(num) {
-  let x = num * 2;
-  return x;
-}
-
-double(6);
-console.log('The value of x is', x, '-- it should be 5.');
+// let x = 5;
+//
+// let double = function(num) {
+//   let x = num * 2;
+//   return x;
+// }
+//
+// double(6);
+// console.log('The value of x is', x, '-- it should be 5.');
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+
+let x = 5;
+
+let double = function(num) {
+  let forcedNum = +num;
+  if(typeof(forcedNum) !== 'number' || isNaN(forcedNum)) {
+    console.log('Need a numerical input to double. Your input: "' + num + '"');
+    return;
+  }
+  let x = forcedNum * 2;
+  return x;
+}
+
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
